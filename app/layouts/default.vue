9a479e486e7ba0a846052c11592ccd1229c61416
elVengador <dev.jimynicanor@gmail.com>
2026-04-07 11:57:31 -0500

    feat: logout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 const user = useSupabaseUser()
+import { Button } from 'primevue';
+const supabase = useSupabaseClient();
+
+const onLogOut = async () => {
+    try {
+        const { error } = await supabase.auth.signOut();
+        if (error) console.log({ error })
+        else navigateTo("/")
+    } catch (error) {
+        console.error({ error })
+    }
+}
 
 </script>
 
@@ -8,6 +20,10 @@ const user = useSupabaseUser()
         <header class="w-full p-4 flex items-center justify-between">
             <NuxtLink to="/" class="flex items-center gap-2"><i class="pi pi-prime"></i>My Store</NuxtLink>
             <nav class="flex items-center gap-2">
+                <NuxtLink to="/admin" class="hover:text-green-500">Dashboard</NuxtLink>
+                <!-- <NuxtLink to="/products">Products</NuxtLink> -->
+                <!-- <NuxtLink to="/setup">Set Up</NuxtLink> -->
+                <Button v-if="user" @click="onLogOut" link class="text-white">Log Out</Button>
                 <NuxtLink v-if="!user" to="/login" class="hover:text-green-500">Login</NuxtLink>
                 <NuxtLink v-if="!user" to="/signup" class="hover:text-green-500">Sign Up</NuxtLink>
             </nav>
