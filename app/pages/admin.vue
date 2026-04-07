b0eaf3bffa4ff472cad471a971e5421c70879b16
elVengador <dev.jimynicanor@gmail.com>
2026-04-07 11:54:00 -0500

    feat: protected admin route

--- app/pages/admin.vue
+++ app/pages/admin.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 
+definePageMeta({ middleware: 'auth' })
+
 </script>
 
 <template>
     <main class="p-2">
         <h1 class="text-2xl text-center mb-4">Dashboard</h1>
-
     </main>
 </template>
