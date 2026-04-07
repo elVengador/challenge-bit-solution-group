f79d09cfe1cc621343897efe396b453cdcee02ea
elVengador <dev.jimynicanor@gmail.com>
2026-04-07 11:48:31 -0500

    chore: initial setup with tailwind, primevue and supabase

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+    <main class="p-2">
+        <h1 class="text-2xl text-center mb-4">TODOS app in Nuxt</h1>
+        <Button label="hi"/>
+    </main>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+import Aura from '@primeuix/themes/aura';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+    '@nuxtjs/supabase',
+  ],
+  devtools: { enabled: true },
+  css: [
+    'primeicons/primeicons.css'
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  },
+  supabase: {
+    redirect: false
+  }
+})
